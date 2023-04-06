02fb60a7e463a71c00914fe1c345a94e60ed75c8
Augusto Paguaga <augustopaguaga010@live.com.ar>
2023-04-05 22:38:16 -0300

    contador

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import "./App.css";
+import freecodecamplogo from "./imagenes/logoap10.png";
+import Boton from "./componentes/boton";
+import Contador from "./componentes/Contador";
+import { useState } from "react";
+
+function App() {
+  const [numClics, setNumClics] = useState(0);
+
+  const manejarClick = () => {
+    setNumClics(numClics + 1);
+  };
+  const reiniciarContador = () => {
+    setNumClics(0);
+  };
+
+  return (
+    <div className="App">
+      <div className="freecodecamp-logo-contenedor">
+        <img
+          className="freecodecamp-logo"
+          src={freecodecamplogo}
+          alt="logo de freecodecamp"
+        />
+      </div>
+      <div className="contenedor-principal">
+        <Contador numClics={numClics} />
+        <Boton texto="Clic" esBotonDeClick={true} manejarClick={manejarClick} />
+        <Boton
+          texto="Reiniciar"
+          esBotonDeClick={false}
+          manejarClick={reiniciarContador}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
